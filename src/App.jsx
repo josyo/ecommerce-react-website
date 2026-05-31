6af34157d9940c35d82c13fb93b34b1056493af6
Joseph <jolakunle50@gmail.com>
2026-05-31 14:34:56 +0100

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from "./Pages/Home";
-import Auth from "./Pages/Auth";
-import Checkout from "./Pages/Checkout";
-import Navbar from "./Components/Navbar"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
 
+import "./App.css";
+import AuthProvider from "./context/AuthContext";
+import ProductDetails from "./pages/ProductDetails";
+import CartProvider from "./context/CartContext";
 
 function App() {
-
-
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-      
-    </div>
-  )
+    <AuthProvider>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartProvider>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
